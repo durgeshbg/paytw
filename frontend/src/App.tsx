74f4ed3caaa93e76fcbf2797276c1e8d0cf5a9d9
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-02-24 14:02:31 +0530

    add: hookup routes

--- frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className='text-3xl'>Hello</div>;
-}
-
-export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import SignUp from './SignUp.tsx';
+import Dashboard from './Dashboard.tsx';
+import SignIn from './SignIn.tsx';
+import Transfer from './Transfer.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/app' element={<App />} />
+        <Route path='/signin' element={<SignUp />} />
+        <Route path='/signup' element={<SignIn />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/transfer' element={<Transfer />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
